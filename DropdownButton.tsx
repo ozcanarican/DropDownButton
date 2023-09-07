e59bce6f81d64f5525b25f0726ae74abd207fe99
ozcan <me@ozcanarican.com>
2023-09-07 12:04:21 +0300

    initted reuseable dropdown button

---
+++ DropdownButton.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { ReactElement, useState } from "react"
+import "./DropdownButton.css"
+
+type ButtonProps = {
+    text: string,
+    content: ReactElement,
+    showBackground: boolean
+}
+
+export default function DropdownButton({ text, content, showBackground = false }: ButtonProps) {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <>
+            {showBackground && isOpen && <div className="dropdown-background" onClick={()=>setIsOpen(false)}></div>}
+            <div className="dropdown-button">
+                <button className="dropdownbutton" onClick={() => setIsOpen((prev) => !prev)}>
+                    <div className="text">{text}</div>
+                    <div className="icon">
+                        <svg fill="#000000" height="100%" width="100%" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 330 330" xmlSpace="preserve">
+                            <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+	c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
+	s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/>
+                        </svg>
+                    </div>
+                </button>
+                <div className={isOpen ? "dropdown" : "dropdown hiddenmenu"}>
+                    {content}
+                </div>
+            </div>
+        </>
+    )
+}
